13bcb15a2e0fe9118813ab74beb414b66da8a812
Willow Carlson-Huber <willow.ch@protonmail.com>
2021-08-09 10:33:04 -0500

    Added support for all basic node types.

--- src/index.ts
+++ src/index.ts
@@ -72,34 +72,56 @@ function renderNode(node: any): Element {
 	} else {
 		console.log(node);
 		switch (Object.keys(node)[0]) {
-			case "VLayout":
-				let vLayoutElement = document.createElement('div')
-				node.VLayout.map(renderNode).forEach(element => {
-					vLayoutElement.appendChild(element)
-					vLayoutElement.appendChild(document.createElement('br'))
-				})
-				return vLayoutElement
-			case "HLayout":
-				let hLayoutElement = document.createElement('table')
-				node.VLayout.map(renderNode).forEach(element => {
-					let td = document.createElement('td')
-					td.appendChild(element)
-					hLayoutElement.appendChild(td)
-				})
-				return hLayoutElement
-			case "InlineLayout":
-				let ilLayoutElement = document.createElement('div')
-				node.InlineLayout.map(renderNode).forEach(element => {
-					ilLayoutElement.appendChild(element)
-				})
-				return ilLayoutElement
 			case "Text":
 				let textElement = document.createElement('span')
 				textElement.innerText = node.Text
 				return textElement
+			case "VLayout":
+				let vLayoutElement = document.createElement('span')
+				node.VLayout.map(renderNode).forEach(element => {
+					vLayoutElement.appendChild(element)
+					vLayoutElement.appendChild(document.createElement('br'))
+				})
+				vLayoutElement.removeChild(vLayoutElement.children[vLayoutElement.children.length-1])
+				return vLayoutElement
+			case "HLayout":
+				let hLayoutElement = document.createElement('table')
+				hLayoutElement.style.display = 'inline-block'
+				hLayoutElement.classList.add('no-border')
+				let hLayoutRow = document.createElement('tr')
+				hLayoutElement.appendChild(hLayoutRow)
+				node.HLayout.map(renderNode).forEach(element => {
+					let td = document.createElement('td')
+					td.appendChild(element)
+					hLayoutRow.appendChild(td)
+				})
+				return hLayoutElement
+			case "InlineLayout":
+				let ilLayoutElement = document.createElement('span')
+				node.InlineLayout.map(renderNode).forEach(element => {
+					ilLayoutElement.appendChild(element)
+				})
+				return ilLayoutElement
+			case "Table":
+				let tableLayoutElement = document.createElement('table')
+				tableLayoutElement.style.display = 'inline-block'
+				node.Table.map((innerNode: any) => {
+					if (innerNode.HLayout) {
+						let row = document.createElement('tr')
+						innerNode.HLayout.map(renderNode).forEach(element => {
+							let td = document.createElement('td')
+							td.appendChild(element)
+							row.appendChild(td)
+						})
+						tableLayoutElement.appendChild(row)
+					} else {
+						console.error("Unexpected non-HLayout child of Table")
+					}
+				})
+				return tableLayoutElement
 			case "Live":
 				let [liveUrl, liveInterval] = node.Live
-				let liveElement = document.createElement('div')
+				let liveElement = document.createElement('span')
 				liveElement.id = Math.random().toString()
 				let liveUpdater = () => {
 					console.log(liveElement.id);
@@ -111,7 +133,68 @@ function renderNode(node: any): Element {
 				pending_lives.push(setInterval(liveUpdater, liveInterval * 1000))
 				setTimeout(liveUpdater, 0)
 				return liveElement
+			case "Blob":
+				let blobElement = document.createElement('a')
+				let [blobUrl, blobType, blobAlt] = node.Blob;
+				if (blobUrl.startsWith('/')) {
+					let origin = (new URL((document.querySelector('#url') as HTMLInputElement).value)).origin
+					blobUrl = origin + blobUrl
+				}
+				blobElement.href = blobUrl
+				blobElement.innerText = `${blobType} ${blobAlt}`
+				return blobElement
+			case "Menu":
+				let menuLayoutElement = document.createElement('table')
+				menuLayoutElement.style.display = 'inline-block'
+				node.Menu.map(renderNode).forEach(element => {
+					let menuLayoutRow = document.createElement('tr')
+					menuLayoutElement.appendChild(menuLayoutRow)
+					let td = document.createElement('td')
+					td.appendChild(element)
+					menuLayoutRow.appendChild(td)
+				})
+				return menuLayoutElement
+			case "Ref":
+				let [refUrl, refAlt] = node.Ref
+				let refElement = document.createElement('a')
+				refElement.innerText = refAlt
+				if (refUrl.startsWith('/')) {
+					let origin = (new URL((document.querySelector('#url') as HTMLInputElement).value)).origin
+					refUrl = origin + refUrl
+				}
+				refElement.href = refUrl
+				return refElement
+			case "Form":
+				let formLayoutElement = document.createElement('form')
+				let formUrl = node.Form[1]
+				if (formUrl.startsWith('/')) {
+					let origin = (new URL((document.querySelector('#url') as HTMLInputElement).value)).origin
+					formUrl = origin + formUrl
+				}
+				formLayoutElement.action = formUrl
+				let formSubmit = document.createElement('input')
+				formSubmit.type = 'submit'
+				formLayoutElement.appendChild(renderNode({ VLayout: node.Form[0] }))
+				formLayoutElement.appendChild(document.createElement('br'))
+				formLayoutElement.appendChild(formSubmit)
+				return formLayoutElement
+			case "Field":
+				let fieldElement = document.createElement('span')
+				let fieldLabelElement = document.createElement('label')
+				let field = document.createElement('input')
+				let [fieldName, fieldLabel, fieldType] = node.Field
+				let fieldId = Math.random().toString()
+				field.id = fieldId
+				field.type = fieldType
+				field.name = fieldName
+				fieldLabelElement.htmlFor = fieldId
+				fieldLabelElement.innerText = fieldLabel
+				fieldElement.appendChild(fieldLabelElement)
+				fieldElement.appendChild(field)
+				return fieldElement
 			default:
+				console.error("Unsupported element ", Object.keys(node)[0]);
+				
 				let unknownElement = document.createElement('span')
 				unknownElement.innerText = "???"
 				return unknownElement
